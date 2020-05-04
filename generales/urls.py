885818d0a3af11f19406597ac2530e5b177e9165
Christian254 <christianfuentes254@gmail.com>
2020-05-04 09:49:28 -0600

    change password

--- generales/urls.py
+++ generales/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 
-from generales.views import Home, send_email
+from generales.views import Home, send_email, show_profile
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('send_email', send_email, name='send_email'),
+    path('show_profile', show_profile, name='show_profile')
 ]
--- generales/views.py
+++ generales/views.py
@@ -25,6 +25,22 @@ def send_email(request):
         body = 'Usuario: {} \n Email: {} \n Departamento: {} \n Rol: {}'.format(user_name, user_email, user_department,
                                                                                 user_rol)
         # Send mail(Subject, body, from, to, fail)
-        send_mail(subject, body,  os.getenv('EMAIL'), [os.getenv('EMAIL')], fail_silently=False)
+        send_mail(subject, body, os.getenv('EMAIL'), [os.getenv('EMAIL')], fail_silently=False)
         return render(request, 'send_email.html', {})
     return render(request, 'send_email.html', {})
+
+
+def show_profile(request):
+    user = request.user;
+    if request.method == 'POST':
+        current_password = request.POST.get('currentPassword', None)
+        new_password = request.POST.get('newPassword', None)
+        if user.check_password(current_password):
+            user.set_password(new_password)
+            user.save()
+            message = 'Su Contraseña ha sido actualizada'
+            return render(request, 'show_profile.html', {'user': user, 'message': message, 'codigo': 1})
+        else:
+            message = 'Debe digitar su contraseña actual'
+            return render(request, 'show_profile.html', {'user': user, 'message': message, 'codigo': 2})
+    return render(request, 'show_profile.html', {'user': user})
