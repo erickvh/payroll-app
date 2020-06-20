7dc150e9cb20504bfe39f2d6fd5b85a60bce8c97
MWalter777 <waltermarrom777@gmail.com>
2020-06-19 22:50:18 -0600

    menu completo
    menu completo

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -6,16 +6,16 @@ from django.contrib.auth.models import Group
 def FilterMenu(get_response):
 
     def middleware(request):
+        lista =  dict()
         if request.user.username:
             user = User.objects.filter(username=request.user.username).first()
             groups = user.groups.all()
-            lista =  dict()
             for group in groups:
                 menu = group.menu_set.filter(padre=None)
                 for me in menu:
                     for m in Menu.objects.filter(padre=me):
                         lista[me.nombre] = {m.nombre:m.url}
-            request.session['lista'] = lista
+        request.session['lista'] = lista
         response = get_response(request)
         print(request.session['lista'])
         return response
